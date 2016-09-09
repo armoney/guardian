bd0fb8685a548f66c1fa7b7feae59c668fcda43e
armoney <armonarcuri@yahoo.com>
2016-09-08 22:13:18 -0700

    :bug: Conditionally req dotenv

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require_relative 'boot'
 
-require "dotenv"
+require "dotenv" if Rails.env.development? || Rails.env.test?
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
